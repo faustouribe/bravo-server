43a6f9411fd002092c558db750152fecf780fbe1
faustouribe <faustouribe@gmail.com>
2022-09-19 20:17:49 -0500

    Create mariadb.sh

---
+++ mariadb.sh
@@ -0,0 +1,8 @@
+docker run -d \
+  --name=mariadb \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -p 3306:3306 \
+  -v ~/config/mariadb:/config \
+  --restart unless-stopped \
+  lscr.io/linuxserver/mariadb:latest
