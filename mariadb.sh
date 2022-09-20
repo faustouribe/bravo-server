25551fd8509fe336f8cc6e2d26fe089caa36a138
faustouribe <faustouribe@gmail.com>
2022-09-19 20:40:44 -0500

    Added env vars

--- mariadb.sh
+++ mariadb.sh
@@ -4,6 +4,11 @@ docker run -d \
   --name=mariadb \
   -e PUID=1000 \
   -e PGID=1000 \
+  -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PWD \
+  -e TZ=$TIMEZONE \
+  -e MYSQL_DATABASE=$DB_NAME \
+  -e MYSQL_USER=$DB_USER \
+  -e MYSQL_PASSWORD=DB_PWD \
   -p 3306:3306 \
   -v ~/config/mariadb:/config \
   --restart unless-stopped \
