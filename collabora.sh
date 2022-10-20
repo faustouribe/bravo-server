939f76bda7625c1c093f5baccee43a4a662ac979
faustouribe <faustouribe@gmail.com>
2022-10-20 13:01:56 -0500

    removed space

--- collabora.sh
+++ collabora.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -x
 source local
-docker run -d \
+docker run -t -d \
   --name=collabora \
   --net=bravo \
-  -p 9980:9980 \ 
-  -e aliasgroup1=https://cloud\.uribe\.com\.mx:443 \
+  -p 9980:9980 \
+  -e aliasgroup1="https://cloud.uribe.com.mx:443" \
   -e username=$COLLAB_USER \
   -e password=$COLLAB_PWD \
   --restart unless-stopped \
