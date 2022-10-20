f526e3f0915a31726b82d7948da39580095f684b
faustouribe <faustouribe@gmail.com>
2022-10-20 11:21:57 -0500

    Added net and name for container

--- collabora.sh
+++ collabora.sh
@@ -1,4 +1,8 @@
+#!/bin/bash -x
+source local
 docker run -t -d \
+  --name=collabora \
+  --net=bravo \
   -p 127.0.0.1:9980:9980 
   -e -e "aliasgroup1=https://cloud.uribe.com.mx:443" 
   -e "username=admin" 
