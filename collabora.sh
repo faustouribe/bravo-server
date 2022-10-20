b6230a6d290273d847f9f4e8a22caaddad6d4c73
faustouribe <faustouribe@gmail.com>
2022-10-20 12:04:54 -0500

    Update collabora.sh

--- collabora.sh
+++ collabora.sh
@@ -3,9 +3,9 @@ source local
 docker run -t -d \
   --name=collabora \
   --net=bravo \
-  -p 127.0.0.1:9980:9980 
-  -e "aliasgroup1=https://cloud.uribe.com.mx:443" 
-  -e username=$COLLAB_USER 
-  -e password=$COLLAB_PWD 
-  --restart always 
+  -p 9980:9980 \ 
+  -e "aliasgroup1=https://cloud.uribe.com.mx:443" \
+  -e username=$COLLAB_USER \
+  -e password=$COLLAB_PWD \
+  --restart unless-stopped \
 collabora/code
