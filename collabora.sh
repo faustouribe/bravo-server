f99a3e04cbc2b78207b5b73b4ca4936032378dbf
faustouribe <faustouribe@gmail.com>
2022-10-20 12:59:28 -0500

    escaped periods

--- collabora.sh
+++ collabora.sh
@@ -4,7 +4,7 @@ docker run -d \
   --name=collabora \
   --net=bravo \
   -p 9980:9980 \ 
-  -e aliasgroup1="https://cloud.uribe.com.mx:443" \
+  -e aliasgroup1=https://cloud\.uribe\.com\.mx:443 \
   -e username=$COLLAB_USER \
   -e password=$COLLAB_PWD \
   --restart unless-stopped \
